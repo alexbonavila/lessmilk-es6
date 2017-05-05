7a6b0f08d5095a382d1d91514834776c41912cd6
alex <alexbonavila44@gmail.com>
2017-05-05 14:37:38 +0200

    Preload + assets

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -3,33 +3,28 @@ import WebFont from 'webfontloader'
 
 export default class extends Phaser.State {
   init () {
-    this.stage.backgroundColor = '#EDEEC9'
-    this.fontsReady = false
-    this.fontsLoaded = this.fontsLoaded.bind(this)
+    this.stage.backgroundColor = '#3498db'
+    this.preload()
   }
 
   preload () {
-    WebFont.load({
-      google: {
-        families: ['Bangers']
-      },
-      active: this.fontsLoaded
-    })
+    this.load.spritesheet('player', 'assets/player.png', 28, 22);
+    this.load.image('wall', 'assets/wall.png');
+    this.load.image('ground', 'assets/ground.png');
+    this.load.image('dust', 'assets/dust.png');
+    this.load.image('exp', 'assets/exp.png');
+    this.load.image('enemy', 'assets/enemy.png');
+    this.load.image('coin', 'assets/coin.png');
 
-    let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
-    text.anchor.setTo(0.5, 0.5)
-
-    this.load.image('loaderBg', './assets/images/loader-bg.png')
-    this.load.image('loaderBar', './assets/images/loader-bar.png')
-  }
-
-  render () {
-    if (this.fontsReady) {
-      this.state.start('Splash')
+    if (!this.desktop) {
+      this.load.image('right', 'assets/right.png');
+      this.load.image('left', 'assets/left.png');
     }
-  }
+    this.load.image('jump', 'assets/jump.png');
 
-  fontsLoaded () {
-    this.fontsReady = true
+    this.load.audio('dead', ['assets/dead.wav', 'assets/dead.mp3']);
+    this.load.audio('dust', ['assets/dust.wav', 'assets/dust.mp3']);
+    this.load.audio('jump', ['assets/jump.wav', 'assets/jump.mp3']);
+    this.load.audio('coin', ['assets/coin.wav', 'assets/coin.mp3']);
   }
 }
