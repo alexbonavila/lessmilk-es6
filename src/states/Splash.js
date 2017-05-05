ac608149a938f9311de11b231e496779a8862976
alex <alexbonavila44@gmail.com>
2017-05-05 16:39:28 +0200

    Deleting unnecessary things

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -5,15 +5,9 @@ export default class extends Phaser.State {
   init () {}
 
   preload () {
-    this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
-    this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
-    centerGameObjects([this.loaderBg, this.loaderBar])
-
-    this.load.setPreloadSprite(this.loaderBar)
     //
     // load your assets
     //
-    this.load.image('mushroom', 'assets/images/mushroom2.png')
     this.load.spritesheet('player', 'assets/player.png', 28, 22);
     this.load.image('wall', 'assets/wall.png');
     this.load.image('ground', 'assets/ground.png');
